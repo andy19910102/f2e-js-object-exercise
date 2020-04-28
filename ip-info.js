855ec04de0c1316f7c26215c886da6e6bd136932
andy19910102 <andy19910102@gmail.com>
2020-04-28 14:58:09 +0800

    Project init

---
+++ ip-info.js
@@ -0,0 +1,14 @@
+/*
+ * TODO: 串接ipinfo.io服務取得使用者的IP資訊，並顯示結果於網頁上
+ * https://ipinfo.io/
+ * 一個可回傳用戶端IP資訊的公開API服務
+ */
+
+const getIPBtn = document.getElementById('getIPBtn'),
+    reportDiv = document.getElementById('reportDiv');
+
+
+getIPBtn.addEventListener('click', function () {
+    console.log('[按鈕被點擊了]');
+
+});
---
+++ scroll-detector.js
@@ -0,0 +1,26 @@
+// 所有導覽列中的連結nav-link
+const navLinkList = document.querySelectorAll('.navbar a.nav-link'),
+    // 導覽列
+    navbar = document.getElementById('navbar'),
+    // 滑動資訊報告元件
+    scrollReport = document.getElementById('scrollReport');
+
+// TODO: 建立章節資訊查詢表
+/* 
+ * {
+ *    section1: {section: sectionDOM, navLink: navLinkDOM},
+ *    section2: {...}, ...
+ * }
+ */
+
+
+// 綁定視窗(window)的滾動事件(scroll)
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+window.addEventListener('scroll', function () {
+    // TODO: 取得視窗的直向滑動偵測點(scrollY)
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollY
+
+    // TODO: 取得每個章節的所在位置頂邊座標(offsetTop)、底邊座標(offsetTop + offsetHeight)
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetTop
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetHeight
+})
