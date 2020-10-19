01d84ae0e63dce16b4959cc6f507fabb232ae91a
andy19910102 <andy19910102@gmail.com>
2020-10-19 17:41:58 +0800

    Update object && array code

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
-// 建立一個陣列
+// 建立一個陣列(arr1)
+const arr1 = [1, 5, 9, 12, 14, "Hello"];
+console.log("[arr1]", arr1);
+
+// TODO: 試著透過陣列的性質渲染下方的資料至畫面上
+const categoryList = [
+    {
+        id: "1",
+        name: "家用品",
+        color: "#ff4f81"
+    },
+    {
+        id: "2",
+        name: "文具",
+        color: "#007cc0"
+    },
+    {
+        id: "3",
+        name: "食品",
+        color: "#f85a40"
+    },
+    {
+        id: "4",
+        name: "玩具",
+        color: "#8e43e7"
+    },
+    {
+        id: "5",
+        name: "其他",
+        color: "#6a737b"
+    }
+];
--- object.js
+++ object.js
@@ -1,2 +1,8 @@
 // 建立一個使用者
 // 並帶有資料欄位 姓名 年齡 電話 是否是管理者 地址
+const user1 = {
+
+};
+
+console.log('[user1]', user1);
+console.log('[user1的資料型別]', typeof user1);
