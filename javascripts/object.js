d29d83b59a6b6ec1eb1e8e6684d32e75fef8c9fc
andy19910102 <andy19910102@gmail.com>
2023-05-16 18:21:28 +0800

    Add CC object example

--- javascripts/object.js
+++ javascripts/object.js
@@ -6,3 +6,25 @@ const user1 = {
 
 console.log('[user1]', user1);
 console.log('[user1的資料型別]', typeof user1);
+
+
+// 建立一個 Toyota Corolla Cross 物件
+const toyotaCorollaCross = {
+    brand: "Toyota",
+    model: "Corolla Cross",
+    year: 2023,
+    color: "White",
+    price: 25000,
+    priceUnit: "USD",
+    engine: "1.8L 4-cylinder",
+    transmission: "CVT",
+    fuelType: "Gasoline",
+    isFrontWheelDrive: true,
+    seatingCapacity: 5,
+    cargoVolume: "487 liters",
+    fuelEfficiency: {
+        city: "8.0 L/100km",
+        highway: "6.5 L/100km",
+        combined: "7.2 L/100km"
+    }
+};
